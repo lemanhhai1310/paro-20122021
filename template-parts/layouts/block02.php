e740410335a5aa6bee15e468c12b0b947d6b2404
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-12-20 23:10:02 +0700

    feat: update

--- template-parts/layouts/block02.php
+++ template-parts/layouts/block02.php
@@ -60,7 +60,7 @@
             </div>
             <div class="uk-width-expand uk-flex-first@m">
                 <div class="block02__box1" uk-toggle="cls: uk-overflow-auto; mode: media; media: (max-width: 959px)">
-                    <div class="uk-child-width-1-2@m uk-grid-match uk-grid-20 uk-flex-nowrap block02__grid" uk-grid>
+                    <div class="uk-child-width-1-2@m uk-grid-match uk-grid-20-m uk-grid-small uk-flex-nowrap block02__grid" uk-grid>
                         <div class="block02__grid__column">
                             <div class="block02__card uk-card uk-card-default uk-card-body">
                                 <div class="block01__item8">
@@ -104,6 +104,29 @@
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="uk-section-small block02__section">
+    <div class="uk-container">
+        <div class="uk-flex-middle" uk-grid>
+            <div class="uk-width-2-5@m">
+                <div class="block01__item24">
+                    <h3 class="uk-h3 block02__title">Học phí tiết kiệm, thanh toán linh hoạt</h3>
+                </div>
+                <div class="block01__item24">
+                    <div class="block02__desc">Cách nhanh nhất để nói tiếng Anh thành thạo với các lớp học 1-1 theo nhu cầu và trình độ của bạn.</div>
+                </div>
+                <div class="block01__item24">
+                    <button class="uk-button uk-button-primary block02__btn"><span>Bắt đầu học tập</span></button>
+                </div>
+            </div>
+            <div class="uk-width-expand">
+                <div class="block02__box1">
+                    <img class="uk-visible@m" src="images/Frame11077.png" alt="">
+                    <img class="uk-hidden@m uk-width-1-1" src="images/Frame110771.png" alt="">
                 </div>
             </div>
         </div>
