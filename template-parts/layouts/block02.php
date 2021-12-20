4f4a2fe2e3f66fa7b36049904a67ce36aa6d371d
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-12-20 23:05:39 +0700

    feat: update

--- template-parts/layouts/block02.php
+++ template-parts/layouts/block02.php
@@ -44,3 +44,68 @@
         </div>
     </div>
 </div>
+<div class="uk-section-small block02__section">
+    <div class="uk-container">
+        <div class="uk-flex-middle uk-grid-77-m" uk-grid>
+            <div class="uk-width-2-5@m">
+                <div class="block01__item24">
+                    <h3 class="uk-h3 block02__title">Tiếng Anh giao tiếp theo nhu cầu thực tiễn</h3>
+                </div>
+                <div class="block01__item24">
+                    <div class="block02__desc">Cách nhanh nhất để nói tiếng Anh thành thạo với các lớp học 1-1 theo nhu cầu và trình độ của bạn.</div>
+                </div>
+                <div class="block01__item24">
+                    <button class="uk-button uk-button-primary block02__btn"><span>Bắt đầu học tập</span></button>
+                </div>
+            </div>
+            <div class="uk-width-expand uk-flex-first@m">
+                <div class="block02__box1" uk-toggle="cls: uk-overflow-auto; mode: media; media: (max-width: 959px)">
+                    <div class="uk-child-width-1-2@m uk-grid-match uk-grid-20 uk-flex-nowrap block02__grid" uk-grid>
+                        <div class="block02__grid__column">
+                            <div class="block02__card uk-card uk-card-default uk-card-body">
+                                <div class="block01__item8">
+                                    <div class="uk-cover-container">
+                                        <img src="images/Frame1241.png" alt="" uk-cover="">
+                                        <canvas width="300" height="216"></canvas>
+                                    </div>
+                                </div>
+                                <div class="block01__item8">
+                                    <div class="block01__item16">
+                                        <div class="block02__card__title">Khóa giao tiếp thông dụng cho người lớn</div>
+                                    </div>
+                                    <div class="block01__item16">
+                                        <div class="block02__card__txt1">Luyện tập để nói tự tin và trôi chảy với tiếng Anh hội thoại</div>
+                                    </div>
+                                    <div class="block01__item16">
+                                        <div class="block02__card__txt2">Begginer 10 bài học</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block02__grid__column">
+                            <div class="block02__card uk-card uk-card-default uk-card-body">
+                                <div class="block01__item8">
+                                    <div class="uk-cover-container">
+                                        <img src="images/Frame12411.png" alt="" uk-cover="">
+                                        <canvas width="300" height="216"></canvas>
+                                    </div>
+                                </div>
+                                <div class="block01__item8">
+                                    <div class="block01__item16">
+                                        <div class="block02__card__title">Khóa giao tiếp thương mại (Business English)</div>
+                                    </div>
+                                    <div class="block01__item16">
+                                        <div class="block02__card__txt1">Luyện tập để nói tự tin và trôi chảy với tiếng Anh hội thoại</div>
+                                    </div>
+                                    <div class="block01__item16">
+                                        <div class="block02__card__txt2">Intermediate 10 bài học</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
