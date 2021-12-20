0d51cd2cb836dd426a5960acaf3fed3dfcfd4a96
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-12-20 18:29:40 +0700

    feat: block01

--- template-parts/layouts/block01.php
+++ template-parts/layouts/block01.php
@@ -8,14 +8,67 @@
     <div class="uk-position-cover uk-section-xsmall">
         <div class="uk-container">
             <div class="uk-text-center uk-light block01__box1">
-                <h2 class="uk-h2 block01__box1__title">Ứng dụng luyện giao tiếp Tiếng Anh với giáo viên nước ngoài</h2>
-                <div class="block01__box1__txt">Tải app học thử miễn phí ngay!</div>
-                <div class="uk-child-width-auto uk-flex-center uk-flex-middle" uk-grid>
+                <div class="uk-child-width-1-1 uk-grid-16" uk-grid>
                     <div>
-
+                        <h2 class="uk-h2 block01__box1__title">Ứng dụng luyện giao tiếp Tiếng Anh với giáo viên nước ngoài</h2>
                     </div>
                     <div>
-
+                        <div class="block01__box1__txt">Tải app học thử miễn phí ngay!</div>
+                    </div>
+                    <div>
+                        <div class="uk-child-width-auto uk-flex-center uk-flex-middle uk-grid-small" uk-grid>
+                            <div>
+                                <a href=""><img class="block01__box1__img" src="images/Download_on_the_App_Store_Badge_VN_RGB_blk_1002171.png" alt=""></a>
+                            </div>
+                            <div>
+                                <a href=""><img class="block01__box1__img" src="images/googleplay.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="uk-visible@m uk-position-relative" uk-height-viewport>
+    <div class="uk-container">
+        <div class="uk-flex-right" uk-grid>
+            <div>
+                <img class="block01__boxpc__img" src="images/Frame11186.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="uk-position-cover uk-section block01__section">
+        <div class="uk-container">
+            <div uk-grid>
+                <div class="uk-width-1-2">
+                    <div class="block01__item100">
+                        <a href=""><img src="images/logo.png" alt=""></a>
+                    </div>
+                    <div class="block01__item100">
+                        <div class="block01__item32">
+                            <div class="block01__item24">
+                                <h2 class="uk-h2 block01__box1__title">Ứng dụng luyện giao tiếp Tiếng Anh với giáo viên nước ngoài</h2>
+                            </div>
+                            <div class="block01__item24">
+                                <div class="block01__item16">
+                                    <div class="block01__boxpc__txt">Cách nhanh nhất để nói tiếng Anh thành thạo với các lớp học 1-1 theo nhu cầu và trình độ của bạn.</div>
+                                </div>
+                                <div class="block01__item16">
+                                    <div class="block01__box1__txt">Tải app học thử miễn phí ngay!</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block01__item32">
+                            <div class="uk-child-width-auto uk-flex-middle uk-grid-small" uk-grid>
+                                <div>
+                                    <a href=""><img class="block01__box1__img" src="images/Download_on_the_App_Store_Badge_VN_RGB_blk_1002171.png" alt=""></a>
+                                </div>
+                                <div>
+                                    <a href=""><img class="block01__box1__img" src="images/googleplay.png" alt=""></a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
