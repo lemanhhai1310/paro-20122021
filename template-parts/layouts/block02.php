59c73a207c26ff14727e7471213b8b86c1060ee3
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-12-20 22:38:48 +0700

    update

--- template-parts/layouts/block02.php
+++ template-parts/layouts/block02.php
@@ -21,3 +21,26 @@
         </div>
     </div>
 </div>
+<div class="uk-section-small block02__section">
+    <div class="uk-container">
+        <div class="uk-flex-middle" uk-grid>
+            <div class="uk-width-2-5@m">
+                <div class="block01__item24">
+                    <h3 class="uk-h3 block02__title">Thời gian siêu linh hoạt</h3>
+                </div>
+                <div class="block01__item24">
+                    <div class="block02__desc">Cách nhanh nhất để nói tiếng Anh thành thạo với các lớp học 1-1 theo nhu cầu và trình độ của bạn.</div>
+                </div>
+                <div class="block01__item24">
+                    <button class="uk-button uk-button-primary block02__btn"><span>Bắt đầu học tập</span></button>
+                </div>
+            </div>
+            <div class="uk-width-expand">
+                <div class="block02__box1">
+                    <img class="uk-visible@m" src="images/Frame1078.png" alt="">
+                    <img class="uk-hidden@m uk-width-1-1" src="images/Frame10781.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
