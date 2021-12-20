332371fb1c99977461475643ba96f962bb51408a
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-12-20 17:02:25 +0700

    feat: update landingpage

---
+++ template-parts/layouts/block01.php
@@ -0,0 +1,24 @@
+<nav class="block01__navbar uk-navbar-container uk-navbar-transparent uk-hidden@m" uk-navbar>
+    <div class="uk-navbar-center">
+        <a class="uk-navbar-item uk-logo" href="#"><img src="images/logo1.png" alt=""></a>
+    </div>
+</nav>
+<div class="uk-hidden@m uk-background-top-center uk-background-norepeat uk-background-width-1-1 uk-cover-container" uk-height-viewport="offset-top: true" data-src="images/Frame1186.png" uk-img>
+    <canvas width="720" height="1576"></canvas>
+    <div class="uk-position-cover uk-section-xsmall">
+        <div class="uk-container">
+            <div class="uk-text-center uk-light block01__box1">
+                <h2 class="uk-h2 block01__box1__title">Ứng dụng luyện giao tiếp Tiếng Anh với giáo viên nước ngoài</h2>
+                <div class="block01__box1__txt">Tải app học thử miễn phí ngay!</div>
+                <div class="uk-child-width-auto uk-flex-center uk-flex-middle" uk-grid>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
